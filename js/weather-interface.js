bb702713bbc31d3363bb330407a0f52a2cf7fdde
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-12 20:37:50 -0700

    add initial files and complete exercise

---
+++ js/weather-interface.js
@@ -0,0 +1,14 @@
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city, displayHumidity);
+  });
+});
